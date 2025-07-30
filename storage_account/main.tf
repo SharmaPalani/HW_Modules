9912db730b530a24b663a85a975ec47d7b5b8605
sharmapalani <sharmapalani95@gmail.com>
2025-07-29 22:50:37 -0400

    commit

--- storage_account/main.tf
+++ storage_account/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_storage_account" "storage_account" {
   name = var.name
   resource_group_name = var.resource_group_name
   location = var.location
-  account_tier = "standard"
+  account_tier = "Standard"
   account_replication_type = "LRS"
 }
